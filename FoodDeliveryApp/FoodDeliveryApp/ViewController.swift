fc3c92164ed9807ca8258e5bca8f29dc827a50bf
Nadezhda <el.shtolts@yandex.ru>
2024-09-23 23:23:59 +0300

    Created base project and removed storyboard references

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Надежда Капацина on 23.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        view.backgroundColor = .red
+    }
+
+
+}
+
